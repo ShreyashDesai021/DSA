c7dcdc1ff0da4afc6f96d43c47bf2034087e7bbd
ShreyashDesai021 <dshreyash04@gmail.com>
2026-01-25 23:54:02 +0000

    Add searchInsertPos, findFloor, and findCeil methods to BinarySearch class

--- BinarySearch.java
+++ BinarySearch.java
@@ -81,8 +81,71 @@ public class BinarySearch{
         return ans;
     }
 
+    public static int searchInsertPos(int[] arr, int key) {
+        int low = 0;
+        int high = arr.length - 1;
+        int ans = arr.length;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] >= key) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    public static int findFloor(int[] arr,int key){
+        int low = 0;
+        int high = arr.length - 1;
+        int floor = -1;
+
+        while(low <= high){
+            int mid = low + (high-low)/2;
+
+            if (arr[mid] > key) {
+                high = mid - 1;
+            }
+            else{ //equality is checked here cause in this case floor is updated
+                floor = arr[mid]; 
+                low = mid + 1;
+            }
+        }
+        
+
+        return floor;
+    }
+
+    public static int findCeil(int[] arr,int key){
+        int low = 0;
+        int high = arr.length - 1;
+        int ceil = -1;
+
+        while(low <= high){
+            int mid = low + (high-low)/2;
+
+            if (arr[mid] >= key) { // equality is checked here cause in this case ceil is updated
+                ceil = arr[mid];
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        
+
+        return ceil;
+    }
+
+
+
+
     public static void main(String[] args){
-        int[] arr = {2, 3, 4, 10, 40, 50, 60, 70, 80, 90 , 100};
+        int[] arr = {3, 4, 4, 7, 8, 10};
 
         int target = 90;
 
@@ -90,11 +153,17 @@ public class BinarySearch{
 
         //int num = bsRecursive(arr, target, 0, arr.length - 1);
 
-        int key = 5;
+        int key = 1;
 
         //int num = lowerBound(arr, key);
 
-        int num = upperBound(arr, key);
+        //int num = upperBound(arr, key);
+
+        //int num = searchInsertPos(arr, key);
+
+        int num = findFloor(arr,key);
+
+        //int num = findCeil(arr,key);
 
         System.out.println(num);
     }

a1f6e6d355aa1424198d351bf5789f9d8e0ed4b6
ShreyashDesai021 <dshreyash04@gmail.com>
2026-01-26 12:42:59 +0000

    Add searchRange method and helper functions to find first and last occurrences in BinarySearch class

--- BinarySearch.java
+++ BinarySearch.java
@@ -141,13 +141,61 @@ public class BinarySearch{
         return ceil;
     }
 
+    public static int[] searchRange(int[] arr, int target) { //first and last occurence
+        int first = findFirst(arr, target);
+        int last = findLast(arr, target);
+        return new int[]{first, last};
+    }
+
+    private static int findFirst(int[] arr, int target) {
+        int low = 0, high = arr.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {  //we cant use >= coz we have to updaye only if equal
+                ans = mid;
+                high = mid - 1;   // move left
+            } else if (arr[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    private static int findLast(int[] arr, int target) {
+        int low = 0, high = arr.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) { //we cant use >= coz we have to updaye only if equa
+                ans = mid;
+                low = mid + 1;    // move right
+            } else if (arr[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
 
 
 
     public static void main(String[] args){
         int[] arr = {3, 4, 4, 7, 8, 10};
 
-        int target = 90;
+        for(int x : arr){
+            System.out.print(x + " ");
+        }
+        System.out.println("");
+
+        int target = 7;
 
         //int num = bsIterative(arr, target);
 
@@ -161,11 +209,19 @@ public class BinarySearch{
 
         //int num = searchInsertPos(arr, key);
 
-        int num = findFloor(arr,key);
+        //int num = findFloor(arr,key);
 
         //int num = findCeil(arr,key);
 
-        System.out.println(num);
+        //System.out.println(num);
+        int[] res = searchRange(arr,target);
+
+        for(int x : res){
+            System.out.print(x + " ");
+        }
+        System.out.println("");
+
+
     }
 
 }

612d15999677b3ae590148c090f4b7f60b4b07bc
ShreyashDesai021 <dshreyash04@gmail.com>
2026-03-02 01:57:58 +0000

    add nextSmallerElement method and update main for testing

--- SQ2.java
+++ SQ2.java
@@ -26,38 +26,71 @@ public class SQ2{
         return st.isEmpty();
     }
 
-public static int[] nextGreaterEleStack(int[] arr){
-    Stack<Integer> st = new Stack<>();
+    
+    public static int[] nextGreaterEleStack(int[] arr){
+        Stack<Integer> st = new Stack<>();
 
-    int[] res = new int[arr.length];    
+        int[] res = new int[arr.length];    
 
-    for(int i = arr.length - 1;i >= 0;i--){
+        for(int i = arr.length - 1;i >= 0;i--){
 
-        while(!st.isEmpty() && st.peek() <= arr[i]){
-            st.pop();
+            while(!st.isEmpty() && st.peek() <= arr[i]){
+                st.pop();
+            }
+
+            /*
+            if(st.peek() > arr[i]){
+                res[i] = st.peek();
+            }else{
+                res[i] = -1;
+            }
+            */         // wrong becoz if stack is empty → peek() throws exception
+
+            if(st.isEmpty()){
+                res[i] =-1;
+            }else{
+                res[i] = st.peek();
+            }
+
+            st.push(arr[i]);
+            
         }
 
-        /*
-        if(st.peek() > arr[i]){
-            res[i] = st.peek();
-        }else{
-            res[i] = -1;
-        }
-        */         // wrong becoz if stack is empty → peek() throws exception
+        return res;
 
-        if(st.isEmpty()){
-            res[i] =-1;
-        }else{
-            res[i] = st.peek();
+    }
+    // Function to find next smaller element using stack
+    public static int[] nextSmallerElement(int[] arr) {
+        int n = arr.length;
+
+        // Stack to keep potential next smaller elements
+        Stack<Integer> st = new Stack<>();
+
+        // Initialize result array with -1
+        int[] ans = new int[n];
+        Arrays.fill(ans, -1); // we can do if,else also as previous
+
+        // Traverse from right to left
+        for (int i = n - 1; i >= 0; i--) {
+
+            // Pop elements from stack that are not smaller
+            while (!st.isEmpty() && st.peek() >= arr[i]) {
+                st.pop();
+            }
+
+            // If stack not empty, top is the next smaller element
+            if (!st.isEmpty()) {
+                ans[i] = st.peek();
+            }
+
+            // Push current element to stack
+            st.push(arr[i]);
         }
 
-        st.push(arr[i]);
-        
+        // Return result
+        return ans;
     }
 
-    return res;
-
-}
 
 
     public static void main(String[] args) {
@@ -68,7 +101,9 @@ public static int[] nextGreaterEleStack(int[] arr){
 
         int[] arr = {1,3,2,4};
 
-        int[] res = nextGreaterEleStack(arr);
+        //int[] res = nextGreaterEleStack(arr);
+
+        int[] res = nextSmallerElement(arr);
 
         for(int i = 0; i < res.length;i++){
             System.out.print(res[i] + " ");

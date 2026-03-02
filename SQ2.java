98aa431f7f6cb2ff3812497642cd43dcba05e356
ShreyashDesai021 <dshreyash04@gmail.com>
2026-03-02 22:26:32 +0000

    Add trapPrefix method to SQ2 class for calculating trapped rainwater

--- SQ2.java
+++ SQ2.java
@@ -92,6 +92,29 @@ public class SQ2{
     }
 
 
+    public static int trapPrefix(int[] height) { // T = O(3n) , S = O(2n) for prefix and suffix arrays
+        int n = height.length;
+
+        int[] prefix = new int[n];
+        int[] suffix = new int[n];
+
+        prefix[0] = height[0];
+        for(int i = 1; i < n; i++)
+            prefix[i] = Math.max(prefix[i-1], height[i]);
+
+        suffix[n-1] = height[n-1];
+        for(int i = n-2; i >= 0; i--)
+            suffix[i] = Math.max(suffix[i+1], height[i]);
+
+        int water = 0;
+
+        for(int i = 0; i < n; i++)
+            water += Math.min(prefix[i], suffix[i]) - height[i];
+
+        return water;
+    }
+
+
 
     public static void main(String[] args) {
         //String s = "()[{}()]";
@@ -99,17 +122,21 @@ public class SQ2{
 
         //System.out.println(balancedPara(s));
 
-        int[] arr = {1,3,2,4};
+       // int[] arr = {1,3,2,4};
 
         //int[] res = nextGreaterEleStack(arr);
 
-        int[] res = nextSmallerElement(arr);
+        //int[] res = nextSmallerElement(arr);
 
-        for(int i = 0; i < res.length;i++){
-            System.out.print(res[i] + " ");
-        }
+        //for(int i = 0; i < res.length;i++){
+        //    System.out.print(res[i] + " ");
+        //}
 
-        System.out.println("");
+        //System.out.println("");
+
+        int[] height = {4,2,0,3,2,5};
+
+        System.out.println(trapPrefix(height));
 
     }
 }

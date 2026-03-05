66eb9234e62639362e436c5a98d87be8ea433c1a
ShreyashDesai021 <dshreyash04@gmail.com>
2026-03-05 21:40:36 +0000

    Add postfixToInfix method to SQ2 class for converting postfix expressions to infix

--- SQ2.java
+++ SQ2.java
@@ -230,7 +230,30 @@ public class SQ2{
         return new StringBuilder(postfix).reverse().toString();
     }
 
+    public static String postfixToInfix(String postfix){
+        Stack<String> s = new Stack<>();
+        int n = postfix.length();
 
+        // Traverse the postfix expression from left to right
+        for (int i = 0; i < n; i++) {
+            char c = postfix.charAt(i);
+
+            // If the character is an operand, push it to the stack
+            if (Character.isLetterOrDigit(c)) { // to check operand Character.isLetterOrDigit(c) just like isalphanum
+                s.push(String.valueOf(c)); // String.valueOf(c) becoz we want to store char as a string
+            } else {  // now need to check before pop becoz in a valid postfix there will always be two operands already in a the stack before we encounter a operator
+                // Pop two operands from the stack
+                String op2 = s.pop();
+                String op1 = s.pop();
+
+                // Form the new infix expression and push back to stack
+                s.push("(" + op1 + c + op2 + ")");
+            }
+        }
+
+        // The final element in the stack is the result
+        return s.peek();
+    }
 
     public static void main(String[] args) {
         //String s = "()[{}()]";

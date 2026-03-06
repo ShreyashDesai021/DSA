41a2564843d810dbf78ef35b8fd9f3a5a69de7a6
ShreyashDesai021 <dshreyash04@gmail.com>
2026-03-06 22:23:45 +0000

    Add prefixToPostfix method to SQ2 class for converting prefix expressions to postfix

--- SQ2.java
+++ SQ2.java
@@ -305,6 +305,31 @@ public class SQ2{
         return s.peek();
     }
 
+    public static String prefixToPostfix(String prefix) {
+        Stack<String> s = new Stack<>();
+        int n = prefix.length();
+
+        // Traverse the prefix expression from right to left
+        for (int i = n - 1; i >= 0; i--) {
+            char c = prefix.charAt(i);
+
+            // If the character is an operand, push it to the stack
+            if (Character.isLetterOrDigit(c)) {
+                s.push(String.valueOf(c));
+            } else {
+                // Pop two operands from the stack
+                String op1 = s.pop();
+                String op2 = s.pop();
+
+                // Form the new postfix expression and push back to stack
+                s.push(op1 + op2 + c);
+            }
+        }
+
+        // The final element in the stack is the result
+        return s.peek();
+    }
+
     public static void main(String[] args) {
         //String s = "()[{}()]";
         //String s = "()[{}(])";
@@ -343,11 +368,15 @@ public class SQ2{
 
         //String infix = prefixToInfix(prefix);
 
-        String postfix = "AB-DE+F*/";
+        //String postfix = "AB-DE+F*/";
 
-        String infix = postfixToPrefix(postfix);
+        //String infix = postfixToPrefix(postfix);
 
-        System.out.println(infix);
+        String prefix = "/-AB*+DEF";
+
+        String postfix = prefixToPostfix(prefix);
+
+        System.out.println(postfix);
 
     }
 }

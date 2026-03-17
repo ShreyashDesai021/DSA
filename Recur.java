7a9645fe639b6320587c445c46c311e1ab6b1113
ShreyashDesai021 <dshreyash04@gmail.com>
2026-03-17 19:43:36 +0000

    Add myPowBrute method for calculating power of a number with support for negative exponents

--- Recur.java
+++ Recur.java
@@ -33,6 +33,27 @@ public class Recur{
         
     }
 
+    public static double myPowBrute(double x, int n) {
+        // Base case: any number to the power of 0 is 1
+        if (n == 0 || x == 1.0) return 1; 
+        
+        long temp = n; // to avoid integer overflow
+        
+        // Handle negative exponents
+        if (n < 0) {
+            x = 1 / x;
+            temp = -1L * n;
+        }
+
+        double ans = 1;
+
+        for (long i = 0; i < temp; i++) {
+            // Multiply ans by x for n times
+            ans *= x; 
+        }
+        return ans;
+    }
+
     public static void main(String[] args){
         String s = "   -12345";
         System.out.println(atoi(s));

4d36dd6a5666ec22436b3ffc00581ad518a8006e
ShreyashDesai021 <dshreyash04@gmail.com>
2026-05-25 21:02:05 +0000

    Add mergeTwoSortedArrays method to TwoPointer class for merging two sorted arrays

--- TwoPointer.java
+++ TwoPointer.java
@@ -52,13 +52,38 @@ public class TwoPointer{
        return i; // returning unique people
     }
 
+    public void mergeTwoSortedArrays(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1; //last element's index in num1
+        int j = n - 1; //last element's index in num2
+        int k = m + n - 1; // last index in num1 (i.e of extra 0's not the elements)
+
+        while(j >= 0){
+            if(i >= 0 && nums1[i] > nums2[j]){
+                nums1[k--] = nums1[i--];
+            }else{
+                nums1[k--] = nums2[j--];
+            }
+        }   
+    
+    }
+
     public static void main(String[] args){
         int[] arr1 = {12,7,2,15};
         int target = 19;
         //System.out.println(Arrays.toString(TwoSum(arr1, target)));
 
         int[] arr2 = {1,1,2,2,2,3,3};
-        System.out.println(removeDuplicates(arr2));
+        //System.out.println(removeDuplicates(arr2));
+
+        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums2 = {2,5,6};
+        int m = 3;
+        int n = 3;  
+        TwoPointer obj1 = new TwoPointer();
+        obj1.mergeTwoSortedArrays(nums1,m,nums2,n);
+
+        System.out.println(Arrays.toString(nums1));
+
     }
 }
 

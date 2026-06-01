603c3ffb052622774fc09df094bda4c9689cc318
ShreyashDesai021 <dshreyash04@gmail.com>
2026-06-01 23:28:07 +0000

    Add maxProduct method to Kadane class for calculating maximum product subarray

--- Kadane.java
+++ Kadane.java
@@ -38,13 +38,38 @@ class Kadane{
         return minSum;
     }
 
+    public static int maxProduct(int[] nums) {
+        
+        int minEnding = nums[0];
+        int maxEnding = nums[0];
+        int maxProd = nums[0]; // dont consider it Integer.MIN_VALUE as it will fail the case where input is nums = {-2}
+
+        for(int i = 1;i < nums.length;i++){
+
+            int v1 = nums[i];
+            int v2 = minEnding * nums[i];
+            int v3 = maxEnding * nums[i];
+
+            minEnding = Integer.min(v1,Integer.min(v2,v3));
+            maxEnding = Integer.max(v1,Integer.max(v2,v3));
+
+            maxProd = Integer.max(maxProd,maxEnding);
+
+        }
+
+        return maxProd;
+
+    }
 
     public static void main(String[] args){
         // int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
         // System.out.println(maxSubArray(nums));
 
-        int[] nums = {1, 2, 3, -4, 5};
-        System.out.println(smallestSumSubarray(nums));
+        // int[] nums = {1, 2, 3, -4, 5};
+        // System.out.println(smallestSumSubarray(nums));
+        
+        int[] nums = {2, 3, -2, 4};
+        System.out.println(maxProduct(nums));
 
     }
 }

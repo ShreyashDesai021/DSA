c2ff5eff339763d693c0e45622895473f374a9f5
ShreyashDesai021 <dshreyash04@gmail.com>
2026-06-02 23:26:17 +0000

    Add maximumSum and maxSubarraySumCircular methods to Kadane class for advanced subarray sum calculations

--- Kadane.java
+++ Kadane.java
@@ -86,6 +86,68 @@ class Kadane{
         return maxAbsSum;
     }
 
+    public static int maximumSum(int[] arr) {
+        
+        int noDelete = arr[0];
+        int oneDelete = 0; // becoz this is not possible for i = 0; that we have one deleted so we have put a void value in it
+        int maxSum = arr[0];
+
+        for(int i = 1;i < arr.length;i++){
+
+            int prev_noDelete = noDelete; // storing it becoz we still need it further for one delete calculation
+            int v1 = noDelete + arr[i];
+            int v2 = arr[i];
+            noDelete = Integer.max(v1,v2);
+
+            int v3 = oneDelete + arr[i];
+            int v4 = prev_noDelete;
+
+            oneDelete = Integer.max(v3,v4);
+
+            maxSum = Integer.max(maxSum,Integer.max(noDelete,oneDelete));
+
+        }
+
+        return maxSum;
+
+    }
+
+    public static int maxSubarraySumCircular(int[] arr) {
+        
+        int minSum =  arr[0];
+        int maxSum = arr[0];
+        int total = arr[0];
+
+        int globalMinSum = arr[0];
+        int globalMaxSum = arr[0];
+
+        for(int i = 1;i < arr.length;i++){
+
+            total += arr[i];
+
+            int v1 = minSum + arr[i];
+            int v2 = arr[i];
+
+            minSum = Integer.min(v1,v2);
+
+            int v3 = maxSum + arr[i];
+            int v4 = arr[i];
+
+            maxSum = Integer.max(v3,v4);
+
+            globalMinSum = Integer.min(globalMinSum,minSum);
+            globalMaxSum = Integer.max(globalMaxSum,maxSum);
+
+        }
+
+        if(globalMaxSum < 0)
+            return globalMaxSum;
+
+        int maxSumCir = Integer.max(globalMaxSum,total - globalMinSum);
+
+        return maxSumCir;
+    }
+
     public static void main(String[] args){
         // int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
         // System.out.println(maxSubArray(nums));
@@ -96,8 +158,17 @@ class Kadane{
         // int[] nums = {2, 3, -2, 4};
         // System.out.println(maxProduct(nums));
 
-        int[] nums = {1, -3, 2, 1, -1};
-        System.out.println(maxAbsoluteSum(nums));
+        // int[] nums = {1, -3, 2, 1, -1};
+        // System.out.println(maxAbsoluteSum(nums));
+
+        // int[] nums = {1, -2, 0, 3};
+        // System.out.println(maximumSum(nums));
+
+        // int[] nums = {50, -2, 50, -2};
+        // System.out.println(maxSubarraySumCircular(nums));
+
+        int[] nums = {-2, -3, -1};
+        System.out.println(maxSubarraySumCircular(nums));
 
     }
 }

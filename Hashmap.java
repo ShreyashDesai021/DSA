3241e46e23c32c086c398e03fa0d5e8b5a190bc2
ShreyashDesai021 <dshreyash04@gmail.com>
2026-06-29 20:43:39 +0000

    Add canConstruct method to Hashmap class for checking if ransomNote can be constructed from magazine

--- Hashmap.java
+++ Hashmap.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Hashmap{
 
-        public static int firstUniqChar(String s) {
+    public static int firstUniqChar(String s) {
 
         HashMap<Character,Integer> map = new HashMap<>();
 
@@ -20,7 +20,32 @@ public class Hashmap{
         return -1;
     }
 
+    public static boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        
+        for(int i = 0;i < magazine.length();i++){
+            char ch = magazine.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+
+        for(int i = 0;i < ransomNote.length();i++){
+            char ch = ransomNote.charAt(i);
+            if(map.containsKey(ch)){
+                map.put(ch,map.getOrDefault(ch,0)-1);
+                if(map.get(ch) == 0){
+                    map.remove(ch);
+                }
+            }else{
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
         System.out.println(firstUniqChar("leetcode")); // Output: 0
+
+        System.out.println(canConstruct("aa", "aab")); // Output: true
     }
 }

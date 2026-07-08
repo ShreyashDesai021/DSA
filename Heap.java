228746e402af1392899131a9252e4197de1de304
ShreyashDesai021 <dshreyash04@gmail.com>
2026-07-08 22:18:10 +0000

    Add findKthLargest method to Heap class for finding the k-th largest element in an array

--- Heap.java
+++ Heap.java
@@ -21,10 +21,32 @@ class Heap {
         return pq.peek();
     }
 
+    public int findKthLargest(int[] arr, int k) {
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        // Insert first k elements
+        for (int i = 0; i < k; i++) {
+            pq.add(arr[i]);
+        }
+
+        // Process remaining elements
+        for (int i = k; i < arr.length; i++) {
+            if (arr[i] > pq.peek()) {
+                pq.poll();
+                pq.add(arr[i]);
+            }
+        }
+
+        return pq.peek();
+    }
+
     public static void main(String[] args) {
         Heap heap = new Heap();
         int[] arr = {7, 10, 4, 3, 20, 15};
         int k = 3;
         System.out.println("Kth smallest element is: " + heap.kthSmallest(arr, k));
     }
+
+    
 }

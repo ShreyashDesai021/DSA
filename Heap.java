bb97e54d19dba115dc1236d1db36f848bb6347cf
ShreyashDesai021 <dshreyash04@gmail.com>
2026-07-08 22:13:59 +0000

    Add Heap class with kthSmallest method for finding the k-th smallest element in an array

---
+++ Heap.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Heap {
+    public int kthSmallest(int[] arr, int k) {
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        // Insert first k elements
+        for (int i = 0; i < k; i++) {
+            pq.add(arr[i]);
+        }
+
+        // Process remaining elements
+        for (int i = k; i < arr.length; i++) {
+            if (arr[i] < pq.peek()) {
+                pq.poll();
+                pq.add(arr[i]);
+            }
+        }
+
+        return pq.peek();
+    }
+
+    public static void main(String[] args) {
+        Heap heap = new Heap();
+        int[] arr = {7, 10, 4, 3, 20, 15};
+        int k = 3;
+        System.out.println("Kth smallest element is: " + heap.kthSmallest(arr, k));
+    }
+}

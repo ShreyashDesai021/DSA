f745328158bf2edc12992c7f651f808a05c1a1ff
ShreyashDesai021 <dshreyash04@gmail.com>
2026-07-10 18:25:50 +0000

    Add Pair class for managing pairs of integers and strings, and implement various min/max heap examples in Heap class

--- Heap.java
+++ Heap.java
@@ -1,6 +1,21 @@
 import java.util.*;
 
-class Heap {
+class Pair{
+    int first;
+    String second;
+
+    Pair(int f,String s){
+        first = f;
+        second = s;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + first + ", " + second + ")";
+    }
+}
+
+public class Heap {
     public int kthSmallest(int[] arr, int k) {
 
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
@@ -42,11 +57,90 @@ class Heap {
     }
 
     public static void main(String[] args) {
-        Heap heap = new Heap();
-        int[] arr = {7, 10, 4, 3, 20, 15};
-        int k = 3;
-        System.out.println("Kth smallest element is: " + heap.kthSmallest(arr, k));
+        // Heap heap = new Heap();
+        // int[] arr = {7, 10, 4, 3, 20, 15};
+        // int k = 3;
+        // System.out.println("Kth smallest element is: " + heap.kthSmallest(arr, k));
+    
+        //Min Heap on First, Min Heap in Second
+
+        System.out.println("Min Heap on First, Min Heap in Second");
+
+        PriorityQueue<Pair> pq1 = new PriorityQueue<>(
+            (a,b) -> {
+                if(a.first != b.first){
+                    return a.first - b.first;
+                }
+                return a.second.compareTo(b.second);
+            }
+        );
+
+        pq1.add(new Pair(1,"apple"));
+        pq1.add(new Pair(2, "banana"));
+        pq1.add(new Pair(1,"guava"));
+        pq1.add(new Pair(1,"aardvark"));
+
+        while (!pq1.isEmpty())
+            System.out.println(pq1.poll());
+
+            
+        System.out.println("Min Heap on First, Max Heap on Second");    
+
+        PriorityQueue<Pair> pq2 = new PriorityQueue<>(
+            (a, b) -> {
+                if (a.first != b.first)
+                    return a.first - b.first;
+
+                return b.second.compareTo(a.second);
+            }
+        );
+
+        pq2.add(new Pair(1, "apple"));
+        pq2.add(new Pair(2, "banana"));
+        pq2.add(new Pair(1, "guava"));
+        pq2.add(new Pair(1, "aardvark"));
+
+        while (!pq2.isEmpty())
+            System.out.println(pq2.poll());
+
+        System.out.println("Max Heap on First, Min Heap on Second");
+
+        PriorityQueue<Pair> pq3 = new PriorityQueue<>(
+            (a, b) -> {
+                if (a.first != b.first)
+                    return b.first - a.first;
+
+                return a.second.compareTo(b.second);
+            }
+        );
+
+        pq3.add(new Pair(1, "apple"));
+        pq3.add(new Pair(2, "banana"));
+        pq3.add(new Pair(1, "guava"));
+        pq3.add(new Pair(1, "aardvark"));
+
+        while (!pq3.isEmpty())
+            System.out.println(pq3.poll());
+
+        System.out.println("Max Heap on First, Max Heap on Second");
+
+        PriorityQueue<Pair> pq4 = new PriorityQueue<>(
+            (a, b) -> {
+                if (a.first != b.first)
+                    return b.first - a.first;
+
+                return b.second.compareTo(a.second);
+            }
+        );
+
+        pq4.add(new Pair(1, "apple"));
+        pq4.add(new Pair(2, "banana"));
+        pq4.add(new Pair(1, "guava"));
+        pq4.add(new Pair(1, "aardvark"));
+
+        while (!pq4.isEmpty())
+            System.out.println(pq4.poll());
+    
     }
 
-    
 }
